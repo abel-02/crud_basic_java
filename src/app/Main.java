854820e55059a398ecf1d20ca3ba9225788cc7b9
abel-02 <abelaquino02j@gmail.com>
2023-12-27 07:55:41 -0300

    Conexion con SQLite

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,34 @@
 package app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import model.ConexionDB;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Connection conexion = null;
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conexion = DriverManager.getConnection("jdbc:sqlite:prueba1.db");
+			Statement stm = conexion.createStatement();
+			ResultSet rst = stm.executeQuery("SELECT * FROM usuarios;");
+			// Index start in 1
+			while(rst.next()) {
+				System.out.println(rst.getString(1) + " " + rst.getString(2) + " " + rst.getString(3) + " " + rst.getString(4)); 
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
